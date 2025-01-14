42299e8ab0de74bf31e742343ff6dbebb9941acd
vague2k <ilovedrawing056@gmail.com>
2025-01-14 13:37:38 -0500

    add linux support for vimtex

--- nvim/lua/vague/plugins/lang/latex/vimtext.lua
+++ nvim/lua/vague/plugins/lang/latex/vimtext.lua
@@ -1,5 +1,9 @@
 return {
   "lervag/vimtex",
   lazy = false,
-  init = function() vim.g.vimtex_view_method = "skim" end,
+  init = function()
+    local sysname = vim.loop.os_uname().sysname
+    if sysname == "Darwin" then vim.g.vimtex_view_method = "skim" end
+    vim.g.vimtex_view_method = "zathura"
+  end,
 }
