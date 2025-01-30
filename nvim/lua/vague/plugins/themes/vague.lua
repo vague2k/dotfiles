25cadfb2317cd010bcd01cb181d4869887ce0c7f
Alberto Hernandez <albert@Albertos-Laptop.local>
2025-01-30 05:40:06 -0500

    working on colorscheme, old settings are now new defaults

--- nvim/lua/vague/plugins/themes/vague.lua
+++ nvim/lua/vague/plugins/themes/vague.lua
@@ -1,15 +1,4 @@
 return {
   "vague2k/vague.nvim",
   dev = true,
-  config = function()
-    require("vague").setup({
-      style = {
-        boolean = "bold",
-        error = "bold",
-        keyword_return = "italic",
-        builtin_constants = "bold",
-        builtin_types = "bold",
-      },
-    })
-  end,
 }
