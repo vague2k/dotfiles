b8cf76bf9b75f1d1dba3a5ec33579b11ca6e71ca
Alberto Hernandez <albert@Albertos-Laptop.local>
2025-01-30 05:39:51 -0500

    early return on mac check

--- nvim/lua/vague/plugins/lang/latex/vimtext.lua
+++ nvim/lua/vague/plugins/lang/latex/vimtext.lua
@@ -3,7 +3,10 @@ return {
   lazy = false,
   init = function()
     local sysname = vim.loop.os_uname().sysname
-    if sysname == "Darwin" then vim.g.vimtex_view_method = "skim" end
+    if sysname == "Darwin" then
+      vim.g.vimtex_view_method = "skim"
+      return
+    end
     vim.g.vimtex_view_method = "zathura"
   end,
 }
