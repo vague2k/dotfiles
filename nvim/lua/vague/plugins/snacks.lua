7528bfbb5ab3945f6c4d77e8325d0391060a2f4b
vague2k <ilovedrawing056@gmail.com>
2025-02-20 23:31:45 -0500

    use snacks explorer instead of neotree

--- nvim/lua/vague/plugins/snacks.lua
+++ nvim/lua/vague/plugins/snacks.lua
@@ -4,6 +4,9 @@ return {
     local snacks = require("snacks")
 
     snacks.setup({
+      explorer = {
+        replace_netrw = true,
+      },
       lazygit = {},
       input = {
         icon = "::",
@@ -21,11 +24,17 @@ return {
 
     local opts = { noremap = true, silent = true }
 
+    -- file explorer
+    vim.keymap.set("n", "<leader>pv", snacks.explorer.open, opts) -- Find files
+
     -- picker stuff
     vim.keymap.set("n", "<leader>ff", snacks.picker.files, opts) -- Find files
+    vim.keymap.set("n", "<leader>fl", snacks.picker.files, opts) -- Find help
     vim.keymap.set("n", "<leader>fh", snacks.picker.highlights, opts) -- Find highlights
     vim.keymap.set("n", "<leader>fd", snacks.picker.diagnostics, opts) -- Find diagnostics
-    vim.keymap.set("n", "<leader>fg", snacks.picker.grep, opts) -- live grep
+    vim.keymap.set("n", "<leader>fg", snacks.picker.grep, opts) -- liFind helpve grep
+    vim.keymap.set("n", "<leader>fl", snacks.picker.files, opts) -- Find help
+    vim.keymap.set("n", "<leader>pp", ":lua Snacks.picker() <cr>", opts) -- opens a list of pickers to choose from
 
     -- lazygit
     vim.keymap.set("n", "<leader>gg", snacks.lazygit.open, opts)
