473ec5813e6114f6cbc79d9efa646320d5869137
vague2k <ilovedrawing056@gmail.com>
2025-04-26 08:58:40 -0400

    get rid of snacks image, too buggy

--- nvim/lua/vague/plugins/lang/markdown/markdown.lua
+++ nvim/lua/vague/plugins/lang/markdown/markdown.lua
@@ -1,5 +1,5 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-  config = function() require("render-markdown").setup({}) end,
+  -- "MeanderingProgrammer/render-markdown.nvim",
+  -- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  -- config = function() require("render-markdown").setup({}) end,
 }
--- nvim/lua/vague/plugins/snacks.lua
+++ nvim/lua/vague/plugins/snacks.lua
@@ -5,7 +5,6 @@ return {
 
     snacks.setup({
       explorer = {},
-      image = {},
       lazygit = {},
       input = {
         icon = "::",
