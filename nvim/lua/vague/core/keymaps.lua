10f677e61db8ebc484d91a19207c5ba19c037dbb
vague2k <ilovedrawing056@gmail.com>
2025-05-18 07:01:51 -0400

    add lsp code actions to keymaps

--- nvim/lua/vague/core/keymaps.lua
+++ nvim/lua/vague/core/keymaps.lua
@@ -10,6 +10,7 @@ keymap("n", "<C-f>", ":silent !tmux neww tmux-sessionizer<CR>", opts) -- Start a
 keymap("n", "<leader>r", vim.lsp.buf.rename, opts) -- renames a reference
 keymap("n", "gd", vim.lsp.buf.definition, opts) -- goto a definition
 keymap("n", "<leader>d", vim.diagnostic.open_float, opts) -- display diagnostic information
+keymap("n", "<leader>a", vim.lsp.buf.code_action, opts) -- display diagnostic information
 
 -- Insert mode
 keymap("i", "jk", "<ESC>", opts) -- Pressing "jk" in insert mode escapes quickly
