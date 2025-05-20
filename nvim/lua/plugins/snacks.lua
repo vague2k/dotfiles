d0f7585c84e54a2088b55cf3c7893cc89bb2362c
vague2k <ilovedrawing056@gmail.com>
2025-05-19 20:20:38 -0400

    MORE CHANGES

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,15 +1,22 @@
 return {
   "folke/snacks.nvim",
   dev = true,
-  keys = { "<leader>pv", "<leader>ff", "<leader>gg" },
+  keys = { "<leader>ff", "<leader>gg" },
   config = function()
     local snacks = require("snacks")
 
     snacks.setup({
-      explorer = {},
       lazygit = {},
+      bigfile = { size = 1024 * 1024 },
+      zen = {
+        toggles = {
+          dim = false,
+        },
+        win = {
+          width = 100,
+        },
+      },
       indent = {
-        enabled = false,
         indent = { enabled = false },
         animate = {
           duration = {
@@ -56,20 +63,16 @@ return {
 
     local opts = { noremap = true, silent = true }
 
-    -- file explorer
-    vim.keymap.set("n", "<leader>pv", snacks.explorer.open, opts) -- Find files
-
-    -- picker stuff
     vim.keymap.set("n", "<leader>ff", snacks.picker.files, opts) -- Find files
     vim.keymap.set("n", "<leader>fl", snacks.picker.help, opts) -- Find help
     vim.keymap.set("n", "<leader>fh", snacks.picker.highlights, opts) -- Find highlights
     vim.keymap.set("n", "<leader>fd", snacks.picker.diagnostics, opts) -- Find diagnostics
     vim.keymap.set("n", "<leader>fg", snacks.picker.grep, opts) -- Live grep
-    vim.keymap.set("n", "<leader>co", snacks.picker.colorschemes, opts) -- Find help
+    vim.keymap.set("n", "<leader>gf", snacks.picker.git_diff, opts) -- pick through git diffs
+    vim.keymap.set("n", "<leader>co", snacks.picker.colorschemes, opts) -- colorschemes
     vim.keymap.set("n", "<leader>to", function() snacks.picker.todo_comments() end, opts) -- Find Todo comments
     vim.keymap.set("n", "<leader>pp", ":lua Snacks.picker() <cr>", opts) -- opens a list of pickers to choose from
-
-    -- lazygit
+    vim.keymap.set("n", "<leader>zz", snacks.zen.zen, opts) -- zen mode
     vim.keymap.set("n", "<leader>gg", snacks.lazygit.open, opts)
   end,
 }
---
+++ nvim/lua/plugins/tree.lua
@@ -0,0 +1,39 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  keys = { "<leader>pv" },
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      source_selector = {
+        winbar = true,
+        sources = { { source = "filesystem" } },
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_by_name = { "node_modules" },
+        },
+      },
+      event_handlers = {
+        {
+          event = "vim_buffer_enter",
+          handler = function()
+            -- removes disables "set number" in neotree window
+            if vim.bo.filetype == "neo-tree" then vim.cmd([[setlocal fillchars=eob:\ ]]) end
+          end,
+        },
+      },
+    })
+
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<leader>pv", ":Neotree toggle<CR>", opts)
+  end,
+}
