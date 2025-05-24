ffc0b8dc5db9cfc766f08c674fd7bfb3e570e132
vague2k <ilovedrawing056@gmail.com>
2025-05-24 16:57:01 -0400

    update treesitter to main branch instead of masert

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  branch = "main",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     require("nvim-treesitter.install").compilers = { "clang", "gcc" }
-    local treesitter = require("nvim-treesitter.configs")
+    local treesitter = require("nvim-treesitter.config")
     ---@diagnostic disable-next-line
     treesitter.setup({
       ensure_installed = {
