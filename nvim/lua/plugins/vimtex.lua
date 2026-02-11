2ad27d4d5407b71bb112d512f0871423f8bd3ea5
vague2k <ilovedrawing056@gmail.com>
2026-02-10 22:40:23 -0500

    use zathura for wsl latex instead of sumatra
    sumatra was giving me problems

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -8,9 +8,9 @@ return {
       return
     end
     if sysname == "Linux" and os.getenv("WSL_DISTRO_NAME") then
-      vim.g.vimtex_view_method = "general"
-      vim.g.vimtex_view_general_viewer = "/mnt/c/Program\\ Files/SumatraPDF/SumatraPDF.exe"
-      vim.g.vimtex_view_general_options = "-forward-search @tex @line @pdf"
+      vim.g.vimtex_view_method = "zathura_simple"
+      vim.g.vimtex_view_forward_search_on_start = 1
+      vim.g.vimtex_view_general_options = "-reuse-instance -forward-search -inverse-search @tex @line @pdf"
     end
   end,
 }
