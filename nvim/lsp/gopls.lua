1e28d543279b8bd7792199e0c0a177267dc0c7f5
vague2k <ilovedrawing056@gmail.com>
2026-02-18 22:44:34 -0500

    FINALLY MAKE GO SHUTUP

--- nvim/lsp/gopls.lua
+++ nvim/lsp/gopls.lua
@@ -4,6 +4,9 @@ return {
       analyses = {
         unusedparams = true,
         unusedvariables = true,
+        -- because i fucking hate warnings all over my screen
+        ST1000 = false, -- Incorrect or missing package comment
+        ST1020 = false, -- The documentation of an exported function should start with the function's name
       },
       semanticTokens = true,
       semanticTokenTypes = {
